fdf6211c3de3e573051a7df516e3e45cddf18f3e
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-12-14 21:55:31 +0600

    🛠️made CTA to connect()

--- ui_components/landingPage/HeroSection.tsx
+++ ui_components/landingPage/HeroSection.tsx
@@ -22,9 +22,14 @@ import { getFromLocalStorage, isValidReferrer } from "@/utils";
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
+import { useChain } from "@cosmos-kit/react";
+import { chain } from "@/constant";
 
 export const HeroSection = () => {
   const [isValidUser, setIsValidUser] = useState(false);
+
+  const chainContext = useChain(chain);
+  const { connect } = chainContext;
 
   const router = useRouter();
   const query = useSearchParams();
@@ -52,17 +57,24 @@ export const HeroSection = () => {
       <div className="flex-1">
         <h1 className="heading1_black 2xl:!text-[48px] 2xl:!leading-[56px]">
           {heroSecHeadingText} <br />
-          <span className="hero-heading-gradient-text">{heroSecHeadingGradientText}</span>
+          <span className="hero-heading-gradient-text">
+            {heroSecHeadingGradientText}
+          </span>
         </h1>
         <p className="paragraph_semibold mt-6 ">
-          {heroSecDescHeadingText} <span className="paragraph_regular">{heroSecDescText}</span>
+          {heroSecDescHeadingText}{" "}
+          <span className="paragraph_regular">{heroSecDescText}</span>
         </p>
         <div className="mt-1 flex items-center gap-2 mb-10">
-          <p className="text-primary paragraph_regular font-semibold">fulfilled by</p>
+          <p className="text-primary paragraph_regular font-semibold">
+            fulfilled by
+          </p>
           <Link href={assetmantleUrlLink} target={"_blank"}>
             <Image src={icons.assetmantleLogo} alt="assetMantleLogo" />
           </Link>
-          <p className="text-primary paragraph_regular font-semibold">Blockchain</p>
+          <p className="text-primary paragraph_regular font-semibold">
+            Blockchain
+          </p>
         </div>
         <Button
           type="button"
@@ -70,13 +82,18 @@ export const HeroSection = () => {
             isValidUser ? "opacity-100" : "opacity-30"
           } inline-block rounded-full bg-[#396AF6] px-6 pb-2 pt-2.5 2xl:!py-3 2xl:!px-10 font-inter text-md font-medium  leading-normal text-white  transition duration-150 ease-in-out hover:bg-warning-600  focus:bg-warning-600 focus:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.3),0_4px_18px_0_rgba(228,161,27,0.2)]`}
           onClick={() => {
-            handleClick();
+            // handleClick();
+            connect();
           }}
-          disabled={!isValidUser}
+          // disabled={!isValidUser}
         >
           <div className="flex">
             {heroSecBtnText}
-            <IconWrapper iconClassName="arrow_right_alt" className="" iconSize="text-[20px] text-white" />
+            <IconWrapper
+              iconClassName="arrow_right_alt"
+              className=""
+              iconSize="text-[20px] text-white"
+            />
           </div>
         </Button>
 
@@ -85,22 +102,42 @@ export const HeroSection = () => {
           <div className="grid grid-cols-4 gap-6">
             <Link href={twitterUrl} target="_blank">
               <div className="flex flex-col items-center justify-center gap-2">
-                <Image src={icons.twitter} width={28} height={28} alt="twitter" />
+                <Image
+                  src={icons.twitter}
+                  width={28}
+                  height={28}
+                  alt="twitter"
+                />
               </div>
             </Link>
             <Link href={telegramUrl} target="_blank">
               <div className="flex flex-col items-center justify-center gap-2">
-                <Image src={icons.telegram} width={28} height={28} alt="telegram" />
+                <Image
+                  src={icons.telegram}
+                  width={28}
+                  height={28}
+                  alt="telegram"
+                />
               </div>
             </Link>
             <Link href={discordUrl} target="_blank">
               <div className="flex flex-col items-center justify-center  gap-2">
-                <Image src={icons.discordShare} width={28} height={28} alt="linkedin" />
+                <Image
+                  src={icons.discordShare}
+                  width={28}
+                  height={28}
+                  alt="linkedin"
+                />
               </div>
             </Link>
             <Link href={instaUrl} target="_blank">
               <div className="flex flex-col items-center justify-center  gap-2">
-                <Image src={icons.instaShare} width={28} height={28} alt="linkedin" />
+                <Image
+                  src={icons.instaShare}
+                  width={28}
+                  height={28}
+                  alt="linkedin"
+                />
               </div>
             </Link>
 
