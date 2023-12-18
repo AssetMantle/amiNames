3a2d3a15fb8de66cf255416a4433aa57f012e36f
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-12-18 13:30:18 +0600

    💅updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { LandingPage } from "@/ui_components/landingPage/LandingPage";
 
 export default function Home() {
   return (
-    <main className="md:flex  md:flex-col md:items-center md:justify-center block pt-24 md:pt-0">
+    <main className="md:flex md:flex-col md:items-center md:justify-center block pt-24 md:pt-20">
       <LandingPage />
     </main>
   );
