1b10e106a1d030b55270a42b2bf0e616d6894f3f
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-02-19 16:15:27 +0600

    🛠️updated

--- ui_components/MintModal.tsx
+++ ui_components/MintModal.tsx
@@ -29,6 +29,7 @@ import ReCAPTCHA from "react-google-recaptcha";
 import Link from "next/link";
 import { IconWrapper } from "./IconWrapper";
 import { useRouter, useSearchParams } from "next/navigation";
+import Loading from "./Loading";
 
 const MintModal = ({
   isOpen,
@@ -44,6 +45,7 @@ const MintModal = ({
   const recaptchaRef = React.createRef();
   const chainContext = useChain(chain);
   const [loader, setLoader] = useState(false);
+  const [LoadingScreen, setLoadingScreen] = useState(false);
   const [success, setSuccess] = useState(false);
   const [isInsufficientBalance, setIsInsufficientBalance] = useState(false);
   const [transactionData, setTransactonData] = useState({} as any);
@@ -204,216 +206,222 @@ const MintModal = ({
   };
 
   return (
-    <Modal
-      openModal={isOpen}
-      setOpenModal={setOpen}
-      className="rounded-lg"
-      header={""}
-      closeModal={() => {
-        router.push(`/profile/${userName}`);
-        handleClose();
-      }}
-    >
-      {success ? (
-        <>
-          <Image
-            className="object-contain absolute h-8 w-8 top-2 right-2 cursor-pointer"
-            alt="selected"
-            src={icons.close}
-            onClick={
-              loader
-                ? () => {
-                    return;
-                  }
-                : () => handleClose()
-            }
-          />
-          <div className="flex flex-col items-center pt-[30px] pb-8 font-inter">
-            <div className="border-b border-b-[#D4DCE2] flex flex-col items-center pb-6">
-              <Image src={icons.successTick} alt="success" />
-              <p className="font-semibold text-[40px] leading-[48px] text-primary mt-6">
-                Claimed successfully!
-              </p>
-              <a
-                className="mt-4 underline block cursor-pointer"
-                target="_blank"
-                href={`https://explorer.assetmantle.one/transactions/${transactionData.transactionHash}`}
-              >
-                View in explorer
-              </a>
-            </div>
-            <p className="paragraph_regular mt-6">
-              Share the referral link and invite your friends to AMI Names
-            </p>
-
-            <div className="mt-4 flex items-center gap-2 cursor-pointer">
-              <a
-                className=" heading3_semibold leading-6 block cursor-pointer text-[#396AF6]"
-                target="_blank"
-                href={`${window.location.origin}?referral=${userName}`}
-              >
-                {`${window.location.origin}?referral=${userName}`}
-              </a>
-
-              <IconWrapper
-                iconClassName="content_copy"
-                iconSize="text-2xl text-[#396AF6]"
-                onClick={() => {
-                  handleCopy(`${window.location.origin}?referral=${userName}`);
-                }}
-              />
-            </div>
-
-            <p className="paragraph_regular mt-6">or share via</p>
-            <div className="flex items-center justify-center mt-6">
-              <div className="grid grid-cols-4 gap-7">
-                <Link
-                  href={`https://twitter.com/intent/tweet?url=${window.location.origin}?referral=${userName}`}
-                  target="_blank"
-                >
-                  <div className="flex flex-col items-center justify-center gap-2">
-                    <Image
-                      src={icons.twitter}
-                      width={36}
-                      height={36}
-                      alt="twitter"
-                    />
-                    <p className="text-[12px] leading-4 font-medium text-black">
-                      {"Twitter"}
-                    </p>
-                  </div>
-                </Link>
-                <Link
-                  href={`https://telegram.me/share/url?url=${window.location.origin}?referral=${userName}`}
-                  target="_blank"
-                >
-                  <div className="flex flex-col items-center justify-center gap-2">
-                    <Image
-                      src={icons.telegram}
-                      width={36}
-                      height={36}
-                      alt="telegram"
-                    />
-                    <p className="text-[12px] leading-4 font-medium text-black">
-                      {"Telegram"}
-                    </p>
-                  </div>
-                </Link>
-                <Link
-                  href={`https://www.linkedin.com/sharing/share-offsite/?url=${window.location.origin}?referral=${userName}`}
-                  target="_blank"
-                >
-                  <div className="flex flex-col items-center justify-center  gap-2">
-                    <Image
-                      src={icons.linkedin}
-                      width={36}
-                      height={36}
-                      alt="linkedin"
-                    />
-                    <p className="text-[12px] leading-4 font-medium text-black">
-                      {"LinkedIn"}
-                    </p>
-                  </div>
-                </Link>
-                <Link href={warpcastUrl} target="_blank">
-                  <div className="flex flex-col items-center justify-center  gap-2">
-                    <Image
-                      src={icons.warpcast}
-                      width={36}
-                      height={36}
-                      alt="warpcast"
-                    />
-                    <p className="text-[12px] leading-4 font-medium text-black">
-                      {"Warpcast"}
-                    </p>
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </>
-      ) : (
-        <>
-          <Image
-            className="object-contain absolute h-8 w-8 top-2 right-2 cursor-pointer"
-            alt="selected"
-            src={icons.close}
-            onClick={
-              loader
-                ? () => {
-                    return;
-                  }
-                : () => setOpen(false)
-            }
-          />
-          <div className=" mb-4 flex flex-col items-center font-inter">
-            <p className="text-primary text-center text-[20px]">
-              {mintModalHeadingText}
-            </p>
-            <div className="bg-[#EBF0FE] rounded-lg border border-[#88A6FA] mt-8 mb-6 w-[80%] py-5">
-              <p className="text-primary text-center leading-10 text-[32px] font-black">
-                {userName}
-              </p>
-            </div>
-            <div className="p-5 mb-8 text-[16px] w-[80%] border border-[#88A6FA] rounded-lg">
-              {isPremium && (
-                <div className="flex items-center justify-between mb-5">
-                  <p className="text-primary opacity-80">Registration fee</p>
-                  <p className="text-primary opacity-80"> ~0.002 MNTL </p>
-                </div>
-              )}
-              <div className="flex items-center justify-between mb-5">
-                <p className="text-primary opacity-80">Network fee</p>
-                <p className="text-primary opacity-80">~0.3 MNTL</p>
-              </div>
-              <div className="flex items-center justify-between">
-                <p className="text-primary font-medium">Estimated total</p>
-                <p className="text-primary font-medium">
-                  ~{isPremium ? 0.302 : 0.3} MNTL
-                </p>
-              </div>
-            </div>
-            <Button
-              type="button"
-              className={` ${
-                loader || isInsufficientBalance
-                  ? "cursor-not-allowed disabled opacity-30"
-                  : ""
-              } inline-block rounded-full bg-[#396AF6] px-[120px] pb-2 pt-2.5 text-md font-medium  leading-normal text-white  transition duration-150 ease-in-out hover:bg-warning-600  focus:bg-warning-600 focus:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.3),0_4px_18px_0_rgba(228,161,27,0.2)]`}
+    <>
+      <Modal
+        openModal={isOpen}
+        setOpenModal={setOpen}
+        className="rounded-lg"
+        header={""}
+        closeModal={() => {
+          setLoadingScreen(true);
+          router.push(`/profile/${userName}`);
+          handleClose();
+        }}
+      >
+        {success ? (
+          <>
+            <Image
+              className="object-contain absolute h-8 w-8 top-2 right-2 cursor-pointer"
+              alt="selected"
+              src={icons.close}
               onClick={
-                isInsufficientBalance
+                loader
                   ? () => {
                       return;
                     }
-                  : () => handleMint()
+                  : () => handleClose()
               }
-            >
-              {loader ? "Please wait..." : "Claim"}
-            </Button>
-            {isInsufficientBalance && (
-              <p className="mt-2 text-error text-center">
-                Insufficient balance,
-                <br /> wallet should contain minimum balance of 0.3MTNL token to
-                claim.
+            />
+            <div className="flex flex-col items-center pt-[30px] pb-8 font-inter">
+              <div className="border-b border-b-[#D4DCE2] flex flex-col items-center pb-6">
+                <Image src={icons.successTick} alt="success" />
+                <p className="font-semibold text-[40px] leading-[48px] text-primary mt-6">
+                  Claimed successfully!
+                </p>
+                <a
+                  className="mt-4 underline block cursor-pointer"
+                  target="_blank"
+                  href={`https://explorer.assetmantle.one/transactions/${transactionData.transactionHash}`}
+                >
+                  View in explorer
+                </a>
+              </div>
+              <p className="paragraph_regular mt-6">
+                Share the referral link and invite your friends to AMI Names
               </p>
-            )}
 
-            <div className="mt-5 flex items-center gap-1">
-              <Link href={assetmantleUrlLink} target={"_blank"}>
-                <Image src={icons.assetmantleLogo} alt="assetMantleLogo" />
-              </Link>
-              <p className="text-primary font-semibold">Blockchain</p>
+              <div className="mt-4 flex items-center gap-2 cursor-pointer">
+                <a
+                  className=" heading3_semibold leading-6 block cursor-pointer text-[#396AF6]"
+                  target="_blank"
+                  href={`${window.location.origin}?referral=${userName}`}
+                >
+                  {`${window.location.origin}?referral=${userName}`}
+                </a>
+
+                <IconWrapper
+                  iconClassName="content_copy"
+                  iconSize="text-2xl text-[#396AF6]"
+                  onClick={() => {
+                    handleCopy(
+                      `${window.location.origin}?referral=${userName}`
+                    );
+                  }}
+                />
+              </div>
+
+              <p className="paragraph_regular mt-6">or share via</p>
+              <div className="flex items-center justify-center mt-6">
+                <div className="grid grid-cols-4 gap-7">
+                  <Link
+                    href={`https://twitter.com/intent/tweet?url=${window.location.origin}?referral=${userName}`}
+                    target="_blank"
+                  >
+                    <div className="flex flex-col items-center justify-center gap-2">
+                      <Image
+                        src={icons.twitter}
+                        width={36}
+                        height={36}
+                        alt="twitter"
+                      />
+                      <p className="text-[12px] leading-4 font-medium text-black">
+                        {"Twitter"}
+                      </p>
+                    </div>
+                  </Link>
+                  <Link
+                    href={`https://telegram.me/share/url?url=${window.location.origin}?referral=${userName}`}
+                    target="_blank"
+                  >
+                    <div className="flex flex-col items-center justify-center gap-2">
+                      <Image
+                        src={icons.telegram}
+                        width={36}
+                        height={36}
+                        alt="telegram"
+                      />
+                      <p className="text-[12px] leading-4 font-medium text-black">
+                        {"Telegram"}
+                      </p>
+                    </div>
+                  </Link>
+                  <Link
+                    href={`https://www.linkedin.com/sharing/share-offsite/?url=${window.location.origin}?referral=${userName}`}
+                    target="_blank"
+                  >
+                    <div className="flex flex-col items-center justify-center  gap-2">
+                      <Image
+                        src={icons.linkedin}
+                        width={36}
+                        height={36}
+                        alt="linkedin"
+                      />
+                      <p className="text-[12px] leading-4 font-medium text-black">
+                        {"LinkedIn"}
+                      </p>
+                    </div>
+                  </Link>
+                  <Link href={warpcastUrl} target="_blank">
+                    <div className="flex flex-col items-center justify-center  gap-2">
+                      <Image
+                        src={icons.warpcast}
+                        width={36}
+                        height={36}
+                        alt="warpcast"
+                      />
+                      <p className="text-[12px] leading-4 font-medium text-black">
+                        {"Warpcast"}
+                      </p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
             </div>
-          </div>
-          <ReCAPTCHA
-            ref={recaptchaRef}
-            size="invisible"
-            sitekey={siteKey}
-            onChange={onReCAPTCHAChange}
-          />
-        </>
-      )}
-    </Modal>
+          </>
+        ) : (
+          <>
+            <Image
+              className="object-contain absolute h-8 w-8 top-2 right-2 cursor-pointer"
+              alt="selected"
+              src={icons.close}
+              onClick={
+                loader
+                  ? () => {
+                      return;
+                    }
+                  : () => setOpen(false)
+              }
+            />
+            <div className=" mb-4 flex flex-col items-center font-inter">
+              <p className="text-primary text-center text-[20px]">
+                {mintModalHeadingText}
+              </p>
+              <div className="bg-[#EBF0FE] rounded-lg border border-[#88A6FA] mt-8 mb-6 w-[80%] py-5">
+                <p className="text-primary text-center leading-10 text-[32px] font-black">
+                  {userName}
+                </p>
+              </div>
+              <div className="p-5 mb-8 text-[16px] w-[80%] border border-[#88A6FA] rounded-lg">
+                {isPremium && (
+                  <div className="flex items-center justify-between mb-5">
+                    <p className="text-primary opacity-80">Registration fee</p>
+                    <p className="text-primary opacity-80"> ~0.002 MNTL </p>
+                  </div>
+                )}
+                <div className="flex items-center justify-between mb-5">
+                  <p className="text-primary opacity-80">Network fee</p>
+                  <p className="text-primary opacity-80">~0.3 MNTL</p>
+                </div>
+                <div className="flex items-center justify-between">
+                  <p className="text-primary font-medium">Estimated total</p>
+                  <p className="text-primary font-medium">
+                    ~{isPremium ? 0.302 : 0.3} MNTL
+                  </p>
+                </div>
+              </div>
+              <Button
+                type="button"
+                className={` ${
+                  loader || isInsufficientBalance
+                    ? "cursor-not-allowed disabled opacity-30"
+                    : ""
+                } inline-block rounded-full bg-[#396AF6] px-[120px] pb-2 pt-2.5 text-md font-medium  leading-normal text-white  transition duration-150 ease-in-out hover:bg-warning-600  focus:bg-warning-600 focus:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.3),0_4px_18px_0_rgba(228,161,27,0.2)]`}
+                onClick={
+                  isInsufficientBalance
+                    ? () => {
+                        return;
+                      }
+                    : () => handleMint()
+                }
+              >
+                {loader ? "Please wait..." : "Claim"}
+              </Button>
+              {isInsufficientBalance && (
+                <p className="mt-2 text-error text-center">
+                  Insufficient balance,
+                  <br /> wallet should contain minimum balance of 0.3MTNL token
+                  to claim.
+                </p>
+              )}
+
+              <div className="mt-5 flex items-center gap-1">
+                <Link href={assetmantleUrlLink} target={"_blank"}>
+                  <Image src={icons.assetmantleLogo} alt="assetMantleLogo" />
+                </Link>
+                <p className="text-primary font-semibold">Blockchain</p>
+              </div>
+            </div>
+            <ReCAPTCHA
+              ref={recaptchaRef}
+              size="invisible"
+              sitekey={siteKey}
+              onChange={onReCAPTCHAChange}
+            />
+          </>
+        )}
+      </Modal>
+      <Loading />
+    </>
   );
 };
 export default MintModal;
