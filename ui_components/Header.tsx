582f4b7061f37577042fe6e0cee620cf81a4c93f
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-02-19 16:15:42 +0600

    🛠️updated

--- ui_components/Header.tsx
+++ ui_components/Header.tsx
@@ -7,12 +7,14 @@ import { icons } from "@/utils/images";
 import { useChain } from "@cosmos-kit/react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import NameListModal from "./NameListModal";
+import Loading from "./Loading";
 
 export default function Header() {
   const router = useRouter();
+  const pathname = usePathname();
   const query = useSearchParams();
   const { address, connect, disconnect } = useChain(chain);
 
@@ -23,6 +25,8 @@ export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isValidUser, setIsValidUser] = useState(false);
   const queryValue = query.get("referral");
+
+  const [LoadingState, setLoadingState] = useState(false);
 
   const handleDisconnect = async () => {
     const isDisConnected = await disconnect();
@@ -97,66 +101,77 @@ export default function Header() {
     setIsOpen(true);
   };
 
-  return (
-    <header className="fixed flex items-center justify-between gap-4 bg-white w-full left-0 md:px-12 py-5 z-[50] font-inter px-2">
-      <Link href={`/?referral=${queryValue}&home=true`}>
-        <div className="col-span-1 flex items-center gap-2">
-          <Image
-            className=""
-            width={36}
-            height={36}
-            alt="selected"
-            src={icons.amiLogo}
-          />
-          <p className="heading1_black md:!text-[28px] !text-[20px]">
-            {"AMI Names"}
-          </p>
-        </div>
-      </Link>
-      <div className="flex items-center gap-5">
-        <Link className="" href={blogsUrl} target="_blank">
-          <div className="flex items-center justify-center gap-2">
-            <p className="text-[16px] text-black leading-6 font-semibold font-inter py-2">
-              Blog
-            </p>
-          </div>
-        </Link>
-        <Link className="" href={docsUrl} target="_blank">
-          <div className="flex items-center justify-center gap-2">
-            <p className="text-[16px] text-black leading-6 font-semibold font-inter py-2">
-              Docs
-            </p>
-          </div>
-        </Link>
-        {address && isValidUser && (
-          <div
-            className="relative"
-            onClick={() => {
-              router.push(
-                `/claim?referral=${queryValue || namesList?.[0]?.name}`
-              );
-            }}
-          >
-            <p className={`paragraph_semibold cursor-pointer ml-1 text-black`}>
-              Claim Name
-            </p>
-          </div>
-        )}
-        {!address ? (
-          <div className="col-span-1 text-right">
-            <Button
-              type="button"
-              className="inline-block rounded-full bg-[#396AF6] px-6 pb-2 pt-2.5 text-md font-medium  leading-normal text-white  transition duration-150 ease-in-out hover:bg-warning-600  focus:bg-warning-600 focus:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.3),0_4px_18px_0_rgba(228,161,27,0.2)]"
-              onClick={handleClick}
-            >
-              Connect Wallet
-            </Button>
-          </div>
-        ) : null}
+  useEffect(() => {
+    setLoadingState(false);
+    console.log(pathname);
+  }, [pathname]);
 
-        {address && (
-          <div className="flex items-center gap-6">
-            {/* {namesList && namesList.length ? (
+  return (
+    <>
+      <header className="fixed flex items-center justify-between gap-4 bg-white w-full left-0 md:px-12 py-5 z-[50] font-inter px-2">
+        <Link
+          href={`/?referral=${queryValue}&home=true`}
+          onClick={() => setLoadingState(true)}
+        >
+          <div className="col-span-1 flex items-center gap-2">
+            <Image
+              className=""
+              width={36}
+              height={36}
+              alt="selected"
+              src={icons.amiLogo}
+            />
+            <p className="heading1_black md:!text-[28px] !text-[20px]">
+              {"AMI Names"}
+            </p>
+          </div>
+        </Link>
+        <div className="flex items-center gap-5">
+          <Link className="" href={blogsUrl} target="_blank">
+            <div className="flex items-center justify-center gap-2">
+              <p className="text-[16px] text-black leading-6 font-semibold font-inter py-2">
+                Blog
+              </p>
+            </div>
+          </Link>
+          <Link className="" href={docsUrl} target="_blank">
+            <div className="flex items-center justify-center gap-2">
+              <p className="text-[16px] text-black leading-6 font-semibold font-inter py-2">
+                Docs
+              </p>
+            </div>
+          </Link>
+          {address && isValidUser && (
+            <div
+              className="relative"
+              onClick={() => {
+                router.push(
+                  `/claim?referral=${queryValue || namesList?.[0]?.name}`
+                );
+              }}
+            >
+              <p
+                className={`paragraph_semibold cursor-pointer ml-1 text-black`}
+              >
+                Claim Name
+              </p>
+            </div>
+          )}
+          {!address ? (
+            <div className="col-span-1 text-right">
+              <Button
+                type="button"
+                className="inline-block rounded-full bg-[#396AF6] px-6 pb-2 pt-2.5 text-md font-medium  leading-normal text-white  transition duration-150 ease-in-out hover:bg-warning-600  focus:bg-warning-600 focus:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.3),0_4px_18px_0_rgba(228,161,27,0.2)]"
+                onClick={handleClick}
+              >
+                Connect Wallet
+              </Button>
+            </div>
+          ) : null}
+
+          {address && (
+            <div className="flex items-center gap-6">
+              {/* {namesList && namesList.length ? (
               <div
                 className="relative"
                 onClick={() => {
@@ -170,52 +185,53 @@ export default function Header() {
                 </p>
               </div>
             ) : null} */}
-            <p
-              role="presentation"
-              className="flex flex-end items-center justify-center float-right gap-2 w-[180px] text-sm text-primary border border-[#396AF6] rounded-full p-2 py-3 cursor-pointer"
-              onClick={(e) => handlePopoverClick(e)}
-            >
-              <span>{trimAddress(address, 8)}</span>
-              <Image className="" alt="down icon" src={icons.chevronDown} />
-              <Popover
-                isOpen={isPopoverOpen}
-                onClose={() => setIsPopoverOpen(false)}
-                className=""
-                leftPad={18}
-                content={
-                  <>
-                    {namesList &&
-                      Array.isArray(namesList) &&
-                      namesList.length > 0 && (
-                        <Button
-                          type="button"
-                          className="inline-block rounded-full buttonBg px-6 pb-2 pt-2.5 text-sm font-medium leading-normal text-white font-inter w-32 mb-2"
-                          onClick={() =>
-                            router.push(
-                              `/profile/${namesList[0].name}?referral=${
-                                queryValue || namesList?.[0]?.name
-                              }`
-                            )
-                          }
-                        >
-                          Profile
-                        </Button>
-                      )}
-                    <Button
-                      type="button"
-                      className="inline-block rounded-full buttonBg px-6 pb-2 pt-2.5 text-sm font-medium leading-normal text-white font-inter w-32"
-                      onClick={handleDisconnect}
-                    >
-                      Disconnect
-                    </Button>
-                  </>
-                }
-                target={targetElement}
-              />
-            </p>
-          </div>
-        )}
-        {/* <div
+              <p
+                role="presentation"
+                className="flex flex-end items-center justify-center float-right gap-2 w-[180px] text-sm text-primary border border-[#396AF6] rounded-full p-2 py-3 cursor-pointer"
+                onClick={(e) => handlePopoverClick(e)}
+              >
+                <span>{trimAddress(address, 8)}</span>
+                <Image className="" alt="down icon" src={icons.chevronDown} />
+                <Popover
+                  isOpen={isPopoverOpen}
+                  onClose={() => setIsPopoverOpen(false)}
+                  className=""
+                  leftPad={18}
+                  content={
+                    <>
+                      {namesList &&
+                        Array.isArray(namesList) &&
+                        namesList.length > 0 && (
+                          <Button
+                            type="button"
+                            className="inline-block rounded-full buttonBg px-6 pb-2 pt-2.5 text-sm font-medium leading-normal text-white font-inter w-32 mb-2"
+                            onClick={() => {
+                              setLoadingState(true);
+                              router.push(
+                                `/profile/${namesList[0].name}?referral=${
+                                  queryValue || namesList?.[0]?.name
+                                }`
+                              );
+                            }}
+                          >
+                            Profile
+                          </Button>
+                        )}
+                      <Button
+                        type="button"
+                        className="inline-block rounded-full buttonBg px-6 pb-2 pt-2.5 text-sm font-medium leading-normal text-white font-inter w-32"
+                        onClick={handleDisconnect}
+                      >
+                        Disconnect
+                      </Button>
+                    </>
+                  }
+                  target={targetElement}
+                />
+              </p>
+            </div>
+          )}
+          {/* <div
           role="presentation"
           onClick={(e) => {
             handleMenuClick(e);
@@ -258,14 +274,16 @@ export default function Header() {
             target={targetElement}
           />
         </div> */}
-      </div>
-      {namesList && (
-        <NameListModal
-          isOpen={isOpen}
-          setIsOpen={setIsOpen}
-          namesList={namesList}
-        />
-      )}
-    </header>
+        </div>
+        {namesList && (
+          <NameListModal
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            namesList={namesList}
+          />
+        )}
+      </header>
+      {LoadingState && <Loading />}
+    </>
   );
 }
