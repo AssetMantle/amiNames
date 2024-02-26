faa4f46294d323b9787989bbf075f7bfd086aee0
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-02-26 13:08:02 +0600

    🛠️updated

--- ui_components/Loading.tsx
+++ ui_components/Loading.tsx
@@ -1,18 +1,29 @@
 import { icons } from "@/utils/images";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import React, { useEffect } from "react";
 
-export default function Loading() {
+export default function Loading({ Show, setShow }: any) {
+  const pathname = usePathname();
+
   useEffect(() => {
-    document.body.style.overflow = "hidden";
-  }, []);
+    Show
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "auto");
+  }, [Show]);
+
+  useEffect(() => {
+    setShow(false);
+  }, [pathname]);
 
   return (
-    <div className="fixed top-0 left-0 right-0 bottom-0 w-[100dvw] h-[100dvh] flex flex-col items-center justify-center bg-white text-headingText">
-      <span>
-        <Image className="h-8 w-8" alt="loader" src={icons.loader} />
-      </span>
-      Loading...
-    </div>
+    Show && (
+      <div className="fixed top-0 left-0 right-0 bottom-0 w-[100dvw] h-[100dvh] flex flex-col items-center justify-center bg-white text-headingText z-10">
+        <span>
+          <Image className="h-8 w-8" alt="loader" src={icons.loader} />
+        </span>
+        Loading...
+      </div>
+    )
   );
 }
