a730f491b27932c704a3b9ccb4e3feb635c37011
AssetMantle DevOps <devops@assetmantle.one>
2024-11-28 08:53:25 +0530

    added all relevant npm packages

---
+++ src/constant/index.tsx
@@ -0,0 +1,85 @@
+export const rpc = "https://rpc.assetmantle.one/";
+export const restApi = "https://rest.assetmantle.one/";
+export const chain = "assetmantle";
+export const defaultChainDenom = "umntl";
+export const defaultFeeAmount = "2000";
+export const defaultFeeGas = "250000";
+export const heroSecHeadingText = "Unleash the future of";
+export const heroSecHeadingGradientText = "Digital Asset Management";
+export const heroSecDescHeadingText = "Introducing AMI Names - ";
+export const heroSecDescText = (
+  <>
+    Your Exclusive Gateway to Simplified Asset Control, Protecting Your Privacy
+  </>
+);
+export const heroSecBtnText = "Claim your own AMI Name";
+export const heroSecSupportText =
+  "Note: You need to have a referral link to claim AMI Name.";
+export const featureSecHeading = (
+  <>
+    Pioneering Decentralized Verifiable Credentials <br /> in Web3
+  </>
+);
+export const feature1Heading = "Passwordless Login";
+export const feature1Desc =
+  "Enhance security & user experience by eliminating passwords.";
+export const feature2Heading = "Interoperability";
+export const feature2Desc =
+  " Same digital identity across platforms and services, including web2 platforms like Auth0";
+export const feature3Heading = "Beyond Cosmos";
+export const feature3Desc =
+  "In the future, AMI Names will have the potential to extend their utility beyond the cosmos ecosystem.";
+export const referralProgramSecHeading = (
+  <>
+    This is exclusively a referral program at the <br /> moment
+  </>
+);
+export const referralProgramSecDesc = "Join us in spreading the word!";
+export const faqHeading = "Frequently Asked Questions";
+export const twitterUrl = "https://twitter.com/AssetMantle";
+export const telegramUrl = "https://t.me/assetmantlechat";
+export const linkedinUrl = "https://in.linkedin.com/";
+export const discordUrl = "https://discord.com/invite/BSdBQ4495d";
+export const instaUrl = "https://www.instagram.com/assetmantle/";
+export const warpcastUrl = "https://warpcast.com/";
+export const instagramUrl = "https://instagram.com/";
+export const blocktheoryUrl = "https://blocktheory.com/";
+export const blogsUrl =
+  "https://blog.assetmantle.one/2023/09/29/introducing-aminames/";
+export const docsUrl = "https://docs.assetmantle.one/AMIName_Terminology/";
+export const contactUsUrl = "mailto:hello@assetmantle.one";
+export const homePageHeadingText = "Claim your permanant";
+export const homePageHeadingGradientText = "AMI Name";
+export const homePageHeadingFree = "for free!";
+export const mintModalHeadingText = "My AMI Name";
+export const premiumAddress = "mantle1pkkayn066msg6kn33wnl5srhdt3tnu2vuet86j";
+export const reservedAddress = "mantle1cgjgrp3fl2ef98me8v7nywtz4nnvjcx6yqu3t7";
+export const referralText = "Referral link";
+export const referralLink = "https://mantle-web3id.vercel.app/";
+export const assetmantleUrlLink = "https://www.assetmantle.one/";
+
+/* export const websiteUrlRegEx =
+  /^(?:(?:(?:https?|ftp|mailto):)?\/\/(?:www\.)?)?[a-zA-Z0-9-]+(\.[a-zA-Z]{2,})?(:\d+)?(\/[^\s]*)?$/; */
+/* export const websiteUrlRegEx =
+  /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/; */
+export const websiteUrlRegEx =
+  /^(?:https?:\/\/)?(?:www\.)?[a-zA-Z0-9-]+(\.[a-zA-Z]{2,})(:\d+)?(\/[^\s]*)?$/;
+
+// export const wwwUrlRegEx = /^www\.[a-zA-Z0-9-]+(\.[a-zA-Z]{2,})+(\S*)$/;
+export const wwwUrlRegEx = /^www\./i;
+
+export const twitterUrlRegEx =
+  /^(?:https?:\/\/)?(?:www\.)?(?:mobile\.)?twitter\.com\/(?:\w+\/)?(?:i\/profiles\/show\/\d+\/?)?(?:\w+\/?)?(?:status\/\d+\/?(?:with_replies\/?)?)?$/;
+export const twitterHandleRegEx = /^(?:@)?[a-zA-Z0-9_]{1,15}$/;
+
+export const telegramUrlRegEx =
+  /^(?:https?:\/\/)?(?:www\.)?t\.me\/[a-zA-Z0-9_]{5,32}$/;
+export const telegramHandleRegEx = /^(?:@)?[a-zA-Z0-9_]{5,32}$/;
+
+/* export const instagramUrlRegEx =
+  /^https:\/\/(?:www\.)?instagram\.com\/[a-zA-Z0-9_](?:[a-zA-Z0-9_.]{0,28}[a-zA-Z0-9_])?\/?$/; */
+export const instagramUrlRegEx =
+  /^(https?:\/\/)?(www\.)?instagram\.com\/[a-zA-Z0-9_.]{1,30}\/?$/;
+
+export const instagramHandleRegEx =
+  /^(?:@)?[a-zA-Z0-9_](?:[a-zA-Z0-9_.]{0,28}[a-zA-Z0-9_])?$/;
