df27bf0a3368d9674315778920bcf9f74bee5993
AssetMantle DevOps <devops@assetmantle.one>
2024-11-28 18:54:35 +0530

    added more required libraries

--- config/defaults.ts
+++ config/defaults.ts
@@ -1,40 +1,36 @@
-import { assets } from 'chain-registry';
-import { AssetList, Asset } from '@chain-registry/types';
-import { GeneratedType, Registry } from '@cosmjs/proto-signing';
-import { AminoTypes } from '@cosmjs/stargate';
+import { Asset, AssetList } from "@chain-registry/types";
+import { GeneratedType, Registry } from "@cosmjs/proto-signing";
+import { AminoTypes } from "@cosmjs/stargate";
+import { assets } from "chain-registry";
 import {
   cosmosAminoConverters,
   cosmosProtoRegistry,
   cosmwasmAminoConverters,
   cosmwasmProtoRegistry,
-  ibcProtoRegistry,
   ibcAminoConverters,
-  osmosisAminoConverters,
-  osmosisProtoRegistry,
-} from 'osmo-query';
+  ibcProtoRegistry,
+} from "interchain-query";
 
-export const defaultChainName = 'assetmantle'
+export const defaultChainName = "assetmantle";
 
 export const chainassets = assets.find(
   (chain) => chain.chain_name === defaultChainName
 ) as AssetList;
 
 export const coin = chainassets.assets.find(
-  (asset) => asset.base === 'uosmo'
+  (asset) => asset.base === "uosmo"
 ) as Asset;
 
 const protoRegistry: ReadonlyArray<[string, GeneratedType]> = [
   ...cosmosProtoRegistry,
   ...cosmwasmProtoRegistry,
   ...ibcProtoRegistry,
-  ...osmosisProtoRegistry,
 ];
 
 const aminoConverters = {
   ...cosmosAminoConverters,
   ...cosmwasmAminoConverters,
   ...ibcAminoConverters,
-  ...osmosisAminoConverters,
 };
 
 export const registry = new Registry(protoRegistry);
