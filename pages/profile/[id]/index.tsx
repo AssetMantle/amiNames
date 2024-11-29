9b9f39c5491b54ca44366167497f70ff1c31479f
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-11-29 15:09:40 +0600

    🛠️💅📕segregated the profile page

--- pages/profile/[id]/index.tsx
+++ pages/profile/[id]/index.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { readUserSocials } from "@/config/dbApi";
 import Header from "@/layout/Header";
-import Image from "next/image";
+import ProfilePrivateView from "@/views/profile/ProfilePrivateView";
+import ProfilePublicView from "@/views/profile/ProfilePublicView";
 import { useRouter } from "next/router";
-import { QRCodeSVG } from "qrcode.react";
 import React, { Suspense, useEffect, useRef, useState } from "react";
 
 export default function Profile() {
@@ -19,10 +19,6 @@ export default function Profile() {
     setSocialData(socialData);
   }, [PROFILE_NAME]);
 
-  const CreateValidURL = (url: string): any => {
-    return url.startsWith("www") ? `https://${url}` : url;
-  };
-
   return (
     <>
       <Header profileName={PROFILE_NAME} />
@@ -32,271 +28,12 @@ export default function Profile() {
         ref={BODY}
       >
         {IsMyProfile ? (
-          <>
-            <div className="flex flex-col items-center justify-center h-[calc(100dvh-97.02px)] gap-9">
-              <div className="flex items-center justify-center mx-auto relative">
-                <div className="flex aspect-square w-[50px] rounded-[50%] scale-[1.1] origin-left">
-                  <Image
-                    src={"/assets/images/LogoSearch.svg"}
-                    alt="Ami Names"
-                    width={200}
-                    height={200}
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      objectFit: "cover",
-                      objectPosition: "center",
-                    }}
-                  />
-                </div>
-                <div className="flex-1 flex gap-1 pl-9 pr-6 py-2 text-gray-900 border border-[#396AF6] bg-gray-50  rounded-e-3xl -ms-7 hero-heading-gradient-text uppercase font-black text-2xl">
-                  {PROFILE_NAME}
-                </div>
-                <Image
-                  src={"/assets/images/icons/chevron-down.svg"}
-                  alt="Ami Names"
-                  width={16}
-                  height={16}
-                  className="ml-4"
-                />
-              </div>
-              <div className="w-[170px] relative bg-white p-2 rounded-md">
-                <Suspense fallback={"Loading..."}>
-                  <QRCodeSVG
-                    width="100%"
-                    height="100%"
-                    value={`https://names.assetmantle.one/profile/${PROFILE_NAME}?referral=${PROFILE_NAME}`}
-                  />
-                </Suspense>
-              </div>
-              <button
-                onClick={() => {
-                  BODY.current &&
-                    BODY.current.scrollHeight &&
-                    BODY.current.scrollTo(0, BODY.current.scrollHeight * 2);
-                }}
-              >
-                <Image
-                  src={"/assets/images/icons/chevron-down.svg"}
-                  alt="Ami Names"
-                  width={32}
-                  height={32}
-                  className="ml-4 rotate-180"
-                />
-              </button>
-            </div>
-            <div className="flex flex-col items-center justify-center h-[calc(100dvh-97.02px)] gap-9">
-              <div className="flex items-center justify-center mx-auto relative">
-                <div className="flex aspect-square w-[50px] rounded-[50%] scale-[1.1] origin-left">
-                  <Image
-                    src={"/assets/images/LogoSearch.svg"}
-                    alt="Ami Names"
-                    width={200}
-                    height={200}
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      objectFit: "cover",
-                      objectPosition: "center",
-                    }}
-                  />
-                </div>
-                <div className="flex-1 flex gap-1 pl-9 pr-6 py-2 text-gray-900 border border-[#396AF6] bg-gray-50  rounded-e-3xl -ms-7 hero-heading-gradient-text uppercase font-black text-2xl">
-                  {PROFILE_NAME}
-                </div>
-                <Image
-                  src={"/assets/images/icons/chevron-down.svg"}
-                  alt="Ami Names"
-                  width={16}
-                  height={16}
-                  className="ml-4"
-                />
-              </div>
-              <div className="flex flex-col gap-3 w-[min(320px,100%)]">
-                <p
-                  className={`leading-[20px] font-bold text-xl cursor-pointer text-center text-headingText`}
-                >
-                  Manage Social Links
-                </p>
-                <div className="flex flex-col gap-5">
-                  <div className="flex flex-col gap-4">
-                    <div className="flex flex-wrap items-center gap-2">
-                      <div className="flex items-center gap-2 w-full md:w-[min(120px,100%)]">
-                        <Image
-                          width={24}
-                          height={24}
-                          className="my-auto"
-                          alt="Twitter"
-                          src={`/assets/images/icons/twitter.png`}
-                        />{" "}
-                        Twitter:
-                      </div>
-                      <input
-                        type="text"
-                        className="border border-[#6188F8] px-4 py-1 rounded-lg flex-grow"
-                      />
-                      <div className="flex items-center gap-2"></div>
-                    </div>
-                    <div className="flex flex-wrap items-center gap-2">
-                      <div className="flex items-center gap-2 w-full md:w-[min(120px,100%)]">
-                        <Image
-                          width={24}
-                          height={24}
-                          className="my-auto"
-                          alt="telegram"
-                          src={`/assets/images/icons/telegram.png`}
-                        />{" "}
-                        Telegram:
-                      </div>
-                      <input
-                        type="text"
-                        className="border border-[#6188F8] px-4 py-1 rounded-lg flex-grow"
-                      />
-                      <div className="flex items-center gap-2"></div>
-                    </div>
-                    <div className="flex flex-wrap items-center gap-2">
-                      <div className="flex items-center gap-2 w-full md:w-[min(120px,100%)]">
-                        <Image
-                          width={24}
-                          height={24}
-                          className="my-auto"
-                          alt="Instagram"
-                          src={"/assets/images/icons/instagram.svg"}
-                        />{" "}
-                        Instagram:
-                      </div>
-                      <input
-                        type="text"
-                        className="border border-[#6188F8] px-4 py-1 rounded-lg flex-grow"
-                      />
-                      <div className="flex items-center gap-2"></div>
-                    </div>
-                    <div className="flex flex-wrap items-center gap-2">
-                      <div className="flex items-center gap-2 w-full md:w-[min(120px,100%)]">
-                        <Image
-                          width={24}
-                          height={24}
-                          className="my-auto"
-                          alt="Instagram"
-                          src={"/assets/images/icons/globe.svg"}
-                        />{" "}
-                        Website:
-                      </div>
-                      <input
-                        type="text"
-                        className="border border-[#6188F8] px-4 py-1 rounded-lg flex-grow"
-                      />
-                      <div className="flex items-center gap-2"></div>
-                    </div>
-                  </div>
-                  <div className="border-b border-b-[#D4DCE2]"></div>
-                </div>
-              </div>
-              <button
-                onClick={() => {
-                  BODY.current &&
-                    BODY.current.scrollHeight &&
-                    BODY.current.scrollTo(0, 0);
-                }}
-              >
-                <Image
-                  src={"/assets/images/icons/chevron-down.svg"}
-                  alt="Ami Names"
-                  width={32}
-                  height={32}
-                  className="ml-4 "
-                />
-              </button>
-            </div>
-          </>
+          <ProfilePrivateView PROFILE_NAME={PROFILE_NAME} BODY={BODY} />
         ) : (
-          <div className="flex flex-col items-center justify-center h-full gap-9">
-            <div className="flex items-center justify-center mx-auto relative">
-              <div className="flex aspect-square w-[50px] rounded-[50%] scale-[1.1] origin-left">
-                <Image
-                  src={"/assets/images/LogoSearch.svg"}
-                  alt="Ami Names"
-                  width={200}
-                  height={200}
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "cover",
-                    objectPosition: "center",
-                  }}
-                />
-              </div>
-              <div className="flex-1 flex gap-1 pl-9 pr-6 py-2 text-gray-900 border border-[#396AF6] bg-gray-50  rounded-e-3xl -ms-7 hero-heading-gradient-text uppercase font-black text-2xl">
-                {PROFILE_NAME}
-              </div>
-            </div>
-            <div className="flex flex-col gap-3 w-[min(320px,100%)]">
-              {SocialData?.twitter && (
-                <a
-                  target="_blank"
-                  className="flex items-center justify-center gap-2 border rounded-md px-5 py-2 text-white font-medium w-[min(320px,100%)] bg-black"
-                  href={CreateValidURL(SocialData?.twitter)}
-                >
-                  <Image
-                    className=""
-                    width={30}
-                    height={30}
-                    alt="twitter"
-                    src={`/assets/images/icons/twitter.png`}
-                  />{" "}
-                  Twitter
-                </a>
-              )}
-              {SocialData?.telegram && (
-                <a
-                  target="_blank"
-                  className="flex items-center justify-center gap-2 border rounded-md px-5 py-2 text-white font-medium w-[min(320px,100%)] am-ami-bg-telegram"
-                  href={CreateValidURL(SocialData?.telegram)}
-                >
-                  <Image
-                    className=""
-                    width={30}
-                    height={30}
-                    alt="telegram"
-                    src={`/assets/images/icons/telegram.png`}
-                  />{" "}
-                  Telegram
-                </a>
-              )}
-              {SocialData?.instagram && (
-                <a
-                  target="_blank"
-                  className="flex items-center justify-center gap-2 border rounded-md px-5 py-2 text-white font-medium w-[min(320px,100%)] am-ami-bg-instagram"
-                  href={CreateValidURL(SocialData?.instagram)}
-                >
-                  <Image
-                    className=""
-                    width={24}
-                    height={24}
-                    alt="instagram"
-                    src={"/assets/images/icons/instagram-t.svg"}
-                  />{" "}
-                  Instagram
-                </a>
-              )}
-              {SocialData?.website && (
-                <a
-                  target="_blank"
-                  className="flex items-center justify-center gap-2 border rounded-md px-5 py-2 text-white font-medium w-[min(320px,100%)] bg-black"
-                  href={CreateValidURL(SocialData?.website)}
-                >
-                  <Image
-                    className=""
-                    width={24}
-                    height={24}
-                    alt="Website"
-                    src={"/assets/images/icons/globe-w.svg"}
-                  />{" "}
-                  Website
-                </a>
-              )}
-            </div>
-          </div>
+          <ProfilePublicView
+            SocialData={SocialData}
+            profileName={PROFILE_NAME}
+          />
         )}
       </main>
     </>
---
+++ views/profile/ProfilePublicView.tsx
@@ -0,0 +1,108 @@
+import Image from "next/image";
+import React from "react";
+
+export default function ProfilePublicView({
+  profileName,
+  SocialData,
+}: {
+  profileName: string;
+  SocialData: any;
+}) {
+  const CreateValidURL = (url: string): any => {
+    return url.startsWith("www") ? `https://${url}` : url;
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full gap-9">
+      <div className="flex items-center justify-center mx-auto relative">
+        <div className="flex aspect-square w-[50px] rounded-[50%] scale-[1.1] origin-left">
+          <Image
+            src={"/assets/images/LogoSearch.svg"}
+            alt="Ami Names"
+            width={200}
+            height={200}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+        </div>
+        <div className="flex-1 flex gap-1 pl-9 pr-6 py-2 text-gray-900 border border-[#396AF6] bg-gray-50  rounded-e-3xl -ms-7 hero-heading-gradient-text uppercase font-black text-2xl">
+          {profileName}
+        </div>
+      </div>
+      <div className="flex flex-col gap-3 w-[min(320px,100%)]">
+        {SocialData?.twitter && (
+          <a
+            target="_blank"
+            className="flex items-center justify-center gap-2 border rounded-md px-5 py-2 text-white font-medium w-[min(320px,100%)] bg-black"
+            href={CreateValidURL(SocialData?.twitter)}
+            rel="noopener noreferrer"
+          >
+            <Image
+              className=""
+              width={30}
+              height={30}
+              alt="twitter"
+              src={`/assets/images/icons/twitter.png`}
+            />{" "}
+            Twitter
+          </a>
+        )}
+        {SocialData?.telegram && (
+          <a
+            target="_blank"
+            className="flex items-center justify-center gap-2 border rounded-md px-5 py-2 text-white font-medium w-[min(320px,100%)] am-ami-bg-telegram"
+            href={CreateValidURL(SocialData?.telegram)}
+            rel="noopener noreferrer"
+          >
+            <Image
+              className=""
+              width={30}
+              height={30}
+              alt="telegram"
+              src={`/assets/images/icons/telegram.png`}
+            />{" "}
+            Telegram
+          </a>
+        )}
+        {SocialData?.instagram && (
+          <a
+            target="_blank"
+            className="flex items-center justify-center gap-2 border rounded-md px-5 py-2 text-white font-medium w-[min(320px,100%)] am-ami-bg-instagram"
+            href={CreateValidURL(SocialData?.instagram)}
+            rel="noopener noreferrer"
+          >
+            <Image
+              className=""
+              width={24}
+              height={24}
+              alt="instagram"
+              src={"/assets/images/icons/instagram-t.svg"}
+            />{" "}
+            Instagram
+          </a>
+        )}
+        {SocialData?.website && (
+          <a
+            target="_blank"
+            className="flex items-center justify-center gap-2 border rounded-md px-5 py-2 text-white font-medium w-[min(320px,100%)] bg-black"
+            href={CreateValidURL(SocialData?.website)}
+            rel="noopener noreferrer"
+          >
+            <Image
+              className=""
+              width={24}
+              height={24}
+              alt="Website"
+              src={"/assets/images/icons/globe-w.svg"}
+            />{" "}
+            Website
+          </a>
+        )}
+      </div>
+    </div>
+  );
+}
