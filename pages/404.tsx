59cb72b7fd03353dbd7500a572c398778539066f
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-12-05 01:12:22 +0600

    🏷️💅🛠️📕created 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,23 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="h-dvh flex flex-col items-center justify-center gap-4 w-[min(600px,100%)] mx-auto py-6 px-4">
+      <Image
+        src={`/assets/images/404.svg`}
+        alt={"404 Page Not found illustration"}
+        width={400}
+        height={400}
+        className="object-contain w-[min(250px,100%)]"
+      />
+      <h2 className="text-xl">Page Not Found</h2>
+      <Link
+        href="/"
+        className="px-4 py-2 text-md font-semibold bg-[#88A6FA] rounded-lg flex items-center gap-3 text-white"
+      >
+        Go To Home
+      </Link>
     </div>
   );
 }
